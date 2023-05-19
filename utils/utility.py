8750509b6d7a51c394e62dcbcae98ef99274dce9
PLSV7Bhanzu <pavan.lingambudhi@expinfi.com>
2023-05-19 11:31:54 +0530

    adding another change

--- utils/utility.py
+++ utils/utility.py
@@ -17,15 +17,26 @@ def upload_to_s3(filename, bucket_name, object_name):
     return response
 
 def extract_json_from_string(string_with_json):
-    json_pattern = r"<JSON>(.*?)<\/JSON>"
-    json_match = re.search(json_pattern, string_with_json, re.DOTALL)
-    print("json_match: ", json_match)
-    if json_match:
-        extracted_text = json_match.group(1)
-        cleaned_json = re.sub(r"\s+", " ", extracted_text)
-        print("cleaned_json: ", cleaned_json)
-        json_data = json.loads(cleaned_json)
-        print("formatted_json: ", json_data)
-        return json_data
-    else:
-        raise ApplicationException("No JSON found in the string", 400)
+    try:
+        json_pattern = r"<JSON>(.*?)<\/JSON>"
+        json_match = re.search(json_pattern, string_with_json, re.DOTALL)
+        print("json_match: ", json_match)
+        if json_match:
+            extracted_text = json_match.group(1)
+            cleaned_json = re.sub(r"\s+", " ", extracted_text)
+            print("cleaned_json: ", cleaned_json)
+            json_data = json.loads(cleaned_json)
+            print("formatted_json: ", json_data)
+            return json_data
+        else:
+            raise ApplicationException("No JSON found in the string", 400)
+    except Exception as e:
+        return extract_json_from_json_regexp(string_with_json)
+def extract_json_from_json_regexp(string_with_json):
+    start_index = string_with_json.find("[")
+    end_index = string_with_json.rfind("]") + 1
+
+    json_string = string_with_json[start_index:end_index]
+    data = json.loads(json_string)
+    print(data)
+    return data
